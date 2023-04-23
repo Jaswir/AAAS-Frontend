17b870ec08e5d07561a985e1ef856ade471edefd
Jaswir <jaswirraghoe@gmail.com>
2023-04-23 17:34:57 +0200

    MVP lvl1

---
+++ Frontend/src/components/AutiQuoteComponent.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function AutiQuoteComponent() {
+
+    interface AutiQuote {
+        author: string
+        quote: string
+    }
+
+    let initialState: AutiQuote = {
+        author: "",
+        quote: ""
+    }
+
+    const [quote, setQuote] = useState<AutiQuote>(initialState)
+
+    const getQuote = () => {
+        axios.get('http://localhost:5000/api/v1/AutiQuotes/random')
+            .then(response => {
+                setQuote((response.data))
+
+                console.log(response.data)
+
+            }).catch(err => {
+
+                console.log(err)
+
+            })
+    }
+
+    //Only triggers when component mounts
+    useEffect(() => {
+        getQuote()
+
+    }, [])
+
+    return (
+        <>
+
+            <div className='flex flex-col items-start justify-center
+                bg-gray-200 px-5 py-3 max-w-[600px]'>
+
+                {/* <button onClick={getQuote}>Get Quote</button> */}
+
+                <h2 className='text-4xl dark:text-black font-bold text-center py-6'>
+                    AutiQuote:
+                </h2>
+
+                <p>
+                    {quote.quote}
+                </p>
+                <br></br>
+                <p>
+                    - {quote.author}
+                </p>
+
+            </div>
+
+        </>
+
+    )
+
+
+
+
+}
+
+export default AutiQuoteComponent
